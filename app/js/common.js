ce6381aa47886b0b33255e85c9724d1ce0a8b9da
Pavel Otdelnov <p4sh0k@gmail.com>
2018-01-15 16:29:24 +0300

    up

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,16 @@
 $(function() {
 
-	$('select').selectric();
+	$('select').selectric({
+	});
 
 	$('.wrapper').addClass('ready');
 	setTimeout(function(){
 		$('.poll').addClass('ready');
 	},300);
+
+	$('.poll_answer_other').click(function(){
+		$(".poll_answer_other_check input").prop('checked', true);
+		$(".poll_answer_other_input input").focus();
+	});
 
 });
